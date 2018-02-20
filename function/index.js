0cab5cf3c293b68bb0d884eb099d832a31d7576f
dabutvin <butvinik@outlook.com>
2018-02-19 23:13:06 -0800

    adds Watson ToneAnalyzerV3

--- function/index.js
+++ function/index.js
@@ -1,8 +1,31 @@
+var ToneAnalyzerV3 = require('watson-developer-cloud/tone-analyzer/v3');
+
 module.exports = function (context) {
-    var comment = context.bindings.comment;
+    var event = context.bindings.event;
+  	var comment = event.comment;
+    
+
+    context.log(comment.body);
   
-    context.log(comment.action);
-  
+    var tone_analyzer = new ToneAnalyzerV3({
+	  username: process.env['ToneAnalyzerUsername'],
+	  password: process.env['ToneAnalyzerPassword'],
+	  version_date: '2016-05-19'
+	});
+
+    tone_analyzer.tone(
+	{
+	  tone_input: comment.body,
+	  content_type: 'text/plain'
+	},
+	function(err, tone) {
+	  if (err) {
+	    context.log(err);
+	  } else {
+	    context.log(JSON.stringify(tone, null, 2));
+	  }
+	});
+
 
     context.done();
 };
