51af5d2084784aa19563a0c92a9d0cf630626e72
dabutvin <butvinik@outlook.com>
2018-02-19 21:18:20 -0800

    sets up queue + web

--- GitHubHook/function/index.js
+++ GitHubHook/function/index.js
@@ -1,5 +1,11 @@
-module.exports = function (context, data) {
-    context.log('GitHub Webhook triggered!');
-    context.res = { body: 'New GitHub comment: ' };
+module.exports = function (context) {
+    context.log('Node.js queue trigger function processed work item', context.bindings.comment);
+    context.log('queueTrigger =', context.bindingData.queueTrigger);
+    context.log('expirationTime =', context.bindingData.expirationTime);
+    context.log('insertionTime =', context.bindingData.insertionTime);
+    context.log('nextVisibleTime =', context.bindingData.nextVisibleTime);
+    context.log('id =', context.bindingData.id);
+    context.log('popReceipt =', context.bindingData.popReceipt);
+    context.log('dequeueCount =', context.bindingData.dequeueCount);
     context.done();
 };
---
+++ web/server.js
@@ -0,0 +1,49 @@
+var http = require('http');
+var fs = require('fs');
+var storage = require('azure-storage');
+var queueService;
+
+fs.readFile('./GitHubHook/local.settings.json', function(error, content) {
+    if(error) {
+        console.log(error);
+    } else {
+        var settings = JSON.parse(content);
+        queueService = storage.createQueueService(settings.Values.AzureWebJobsStorage);
+
+        queueService.createQueueIfNotExists('comments', function(error) {
+          if (error) {
+            console.log(error);
+          }
+        });
+    }
+});
+
+http.createServer(function (request, response) {
+
+    if(request.url.indexOf('hook') > -1) {
+        response.writeHead(200, { 'Content-Type': 'application/json'});
+
+
+        var objectToSend = { 'data': true };
+
+        queueService.createMessage('comments', new Buffer(JSON.stringify(objectToSend)).toString('base64'), function(error) {
+          if (error) {
+            console.log(error);
+          }
+        });
+        
+        response.end("Success", 'utf-8');
+    } else {
+        fs.readFile('./web/index.html', function(error, content) {
+            if (error) {
+                response.writeHead(500);
+                response.end('Sorry :( ' + error.code);
+                response.end();
+            } else {
+                response.writeHead(200, { 'Content-Type': 'text/html' });
+                response.end(content, 'utf-8');
+            }
+        });
+    }
+
+}).listen(process.env.PORT || 3000);;
